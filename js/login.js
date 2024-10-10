f553151765da87d9c595a6327d59dd1e394e6962
pxlphap <pxlphap@gmail.com>
2024-10-10 15:20:01 +0700

    update form and tab

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 ; (function ($) {
     $(document).ready(function () {
-        $(document).on('click', '.wrap-tab-form button:not(button.active)', function () {
-            $('.wrap-tab-form button').toggleClass('active');
+        $(document).on('click', '.tabs-form button:not(button.active)', function () {
+            $('.tabs-form button').toggleClass('active');
         })
     });
 
