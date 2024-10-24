0c132a6eee647cad87a279ccc5c7cf637f5fed51
pxlphap <pxlphap@gmail.com>
2024-10-24 16:16:54 +0700

    add/popup

--- js/popup.js
+++ js/popup.js
@@ -3,4 +3,12 @@ $(document).ready(function () {
         let idx = $(this).attr('id');
         $('.popup[for="' + idx + '"]').toggleClass('active');
     });
+
+    $(document).on('click', '.popup-close', function () {
+        $(this).parent().toggleClass('active');
+    });
+
+    $(document).on('click', '.support', function () {
+        $('.popup-support').toggleClass('active');
+    });
 });
